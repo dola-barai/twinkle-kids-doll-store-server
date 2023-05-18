a913cf966d6ecf530d5acc80092bc2680c44119c
Dola Barai <dola43dc@gmail.com>
2023-05-19 00:41:57 +0600

    single toy data find

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
@@ -31,7 +31,14 @@ async function run() {
         const cursor = kidsDollCollection.find();
         const result = await cursor.toArray();
         res.send(result);
-    })
+    });
+
+    app.get('/allToys/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await kidsDollCollection.findOne(query);
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

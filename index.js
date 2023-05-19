b96f31fad021491b77b5d0b34ae9ea99a9084710
Dola Barai <dola43dc@gmail.com>
2023-05-19 17:38:03 +0600

    post data get from mongodb

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/addToy', async(req, res) => {
+      const cursor = addedToyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

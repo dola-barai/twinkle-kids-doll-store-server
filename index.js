7d277cb7e34e29f38cb3a71fdc1031a1b306bca2
Dola Barai <dola43dc@gmail.com>
2023-05-19 18:02:08 +0600

    my toys data deleteed

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/addToy/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addedToyCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

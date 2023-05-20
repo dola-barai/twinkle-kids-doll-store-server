cdbaa21524ca49f751ffca45ba735648df8b8bab
Dola Barai <dola43dc@gmail.com>
2023-05-20 13:13:09 +0600

    updated data added

--- index.js
+++ index.js
@@ -41,13 +41,6 @@ async function run() {
         res.send(result);
     });
 
-    app.post('/addToy', async(req, res) => {
-      const addToy = req.body;
-      console.log(addToy);
-      const result = await addedToyCollection.insertOne(addToy);
-      res.send(result)
-    })
-
     app.get('/addToy', async(req, res) => {
       const cursor = addedToyCollection.find();
       const result = await cursor.toArray();
@@ -55,11 +48,42 @@ async function run() {
     });
 
     app.get('/addToy/:id', async(req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const _id = req.params.id;
+      const query = { _id: new ObjectId(_id) }
       const result = await addedToyCollection.findOne(query);
       res.send(result)
+      console.log(result);
     });
+
+    app.post('/addToy', async(req, res) => {
+      const addToy = req.body;
+      console.log(addToy);
+      const result = await addedToyCollection.insertOne(addToy);
+      res.send(result)
+    })
+
+    app.put('/addToy/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedToy = req.body;
+
+      const toy = {
+          $set: {
+              name: updatedToy.name, 
+              quantity: updatedToy.quantity, 
+              supplier: updatedToy.supplier, 
+              taste: updatedToy.taste, 
+              category: updatedToy.category, 
+              details: updatedToy.details, 
+              photo: updatedToy.photo
+          }
+      }
+
+      const result = await addedToyCollection.updateOne(filter, toy, options);
+      res.send(result);
+  })
+
 
     app.delete('/addToy/:id', async(req, res) => {
       const id = req.params.id;
